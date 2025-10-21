6ee34dd02e76b7b3b328b35dd93a52e9ccbfe2f0
08SjoAnt <anton.sjogren@skola.taby.se>
2025-10-21 09:59:16 +0200

    Horbajs

--- v42/rle.py
+++ v42/rle.py
@@ -7,7 +7,18 @@ def rle(string):
         else:
             output += f"{count}{string[i - 1]}"
             count = 1
-        output += f"{count}{string[i - 1]}"
+        output += f"{count}{string[-1]}"
     return(output)
 
-print(rle('BBBBSSCCCEEEEEEEEEEBBBBB'))
+print(rle('BBBBSSCCC'))
+
+def numbers_between(tal1, tal2):
+    if tal1 < tal2:
+        for i in range(tal1 + 1, tal2):
+            print(i)
+    elif tal2 < tal1:
+        for i in range(tal2 + 1, tal1):
+            print(i)
+    return()
+
+numbers_between(1, 12)
